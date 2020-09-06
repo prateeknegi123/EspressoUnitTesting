dd464789fe07c6f4b530e1bb2f96ef266ada0cf9
Prateek <prateek.n@parangat.com>
2020-09-06 01:06:54 -0700

    Room db testing

--- app/src/main/java/com/example/androiduitesting/MainActivity.kt
+++ app/src/main/java/com/example/androiduitesting/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.androiduitesting
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -32,13 +35,13 @@ class MainActivity : AppCompatActivity() {
 
     fun decreaseScore(view: View) {
         val viewID = view.id
-        when(viewID){
-            R.id.decreaseTeam1->{
+        when (viewID) {
+            R.id.decreaseTeam1 -> {
                 mScore1--
                 score_1.text = mScore1.toString()
             }
 
-            R.id.decreaseTeam2->{
+            R.id.decreaseTeam2 -> {
                 mScore2--;
                 score_2.text = mScore2.toString()
             }
@@ -46,12 +49,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun increaseScore(view: View) {
-        when(view.id){
-            R.id.increaseTeam1->{
+        when (view.id) {
+            R.id.increaseTeam1 -> {
                 mScore1++;
                 score_1.text = mScore1.toString()
             }
-            R.id.increaseTeam2->{
+            R.id.increaseTeam2 -> {
                 mScore2++;
                 score_2.text = mScore2.toString()
             }
@@ -60,8 +63,36 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSaveInstanceState(outState: Bundle) {
         // Save the scores.
+
         outState.putInt(STATE_SCORE_1, mScore1)
         outState.putInt(STATE_SCORE_2, mScore2)
         super.onSaveInstanceState(outState)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu);
+        val nightMode = AppCompatDelegate.getDefaultNightMode()
+        if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+            menu!!.findItem(R.id.night_mode).setTitle(R.string.day_mode)
+        } else {
+            menu!!.findItem(R.id.night_mode).setTitle(R.string.night_mode)
+        }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.night_mode) {
+            val nightMode = AppCompatDelegate.getDefaultNightMode()
+            //Set the theme mode for the restarted activity
+            if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            recreate()
+        }
+
+        return true
     }
 }
