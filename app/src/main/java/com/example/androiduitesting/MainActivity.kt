5c9ff27f9441782fb185519a7fa82846cede5e14
Prateek <prateek.n@parangat.com>
2020-09-10 02:06:29 -0700

    Notification

--- app/src/main/java/com/example/androiduitesting/MainActivity.kt
+++ app/src/main/java/com/example/androiduitesting/MainActivity.kt
@@ -4,8 +4,11 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Bundle
+import android.provider.SyncStateContract.Helpers.update
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
@@ -18,20 +21,36 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private val PRIMARY_CHANNEL_ID: String = "primary_notification_channel"
+        private val ACTION_UPDATE_NOTIFICATION: String =
+            "com.example.android.notifyme.ACTION_UPDATE_NOTIFICATION"
+        val ACTION_DELETE_NOTIFICATION: String =
+            "com.example.android.notifyme.ACTION_DELETE_NOTIFICATION"
         private val NOTIFICATION_ID: Int = 0
     }
 
     private lateinit var mNotifyManager: NotificationManager
+    private lateinit var notificationReceiver: NotificationReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        notificationReceiver = NotificationReceiver(this@MainActivity)
         createNotificationChannel()
+        setNotificationButtonState(true, false, false)
         notify.setOnClickListener {
             sendNotification()
         }
 
+        update.setOnClickListener {
+            updateNotification()
+        }
+
+        cancel.setOnClickListener {
+            cancelNotification()
+        }
+
+        registerReceiver(notificationReceiver, IntentFilter(ACTION_UPDATE_NOTIFICATION))
     }
 
     private fun createNotificationChannel() {
@@ -51,10 +70,29 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun sendNotification() {
+        val updateIntent = Intent(ACTION_UPDATE_NOTIFICATION)
+        val updatePendingIntent = PendingIntent.getBroadcast(
+            this,
+            NOTIFICATION_ID, updateIntent, PendingIntent.FLAG_ONE_SHOT
+        )
+
+        val deleteIntent = Intent(ACTION_DELETE_NOTIFICATION)
+        val deletePendingIntent = PendingIntent.getBroadcast(
+            this,
+            NOTIFICATION_ID, deleteIntent, PendingIntent.FLAG_ONE_SHOT
+        )
+
         val notifyBuilder = getNotificationBuilder()
+
+        notifyBuilder
+            .addAction(R.drawable.ic_action_name, "Update", updatePendingIntent)
+            .setDeleteIntent(deletePendingIntent)
+
+
         mNotifyManager.notify(
             NOTIFICATION_ID, notifyBuilder.build()
         )
+        setNotificationButtonState(false, true, true)
     }
 
     private fun getNotificationBuilder(): NotificationCompat.Builder {
@@ -62,8 +100,9 @@ class MainActivity : AppCompatActivity() {
             .setContentTitle("You've been notified!")
             .setContentText("This is your notification text.")
             .setSmallIcon(R.mipmap.ic_launcher)
-            .setContentIntent(pendingIntent())
             .setAutoCancel(true)
+            .setContentIntent(pendingIntent())
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
     }
 
     private fun pendingIntent(): PendingIntent {
@@ -74,6 +113,35 @@ class MainActivity : AppCompatActivity() {
             notificationIntent,
             PendingIntent.FLAG_UPDATE_CURRENT
         )
+    }
+
+    fun updateNotification() {
+        val androidImage = BitmapFactory
+            .decodeResource(resources, R.drawable.mascot_1)
+
+        val notifyBuilder = getNotificationBuilder()
+        notifyBuilder.setStyle(
+            NotificationCompat.BigPictureStyle()
+                .bigPicture(androidImage)
+                .setBigContentTitle("Notification Updated!")
+        )
+        mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build())
+        setNotificationButtonState(false, false, true)
+    }
+
+    fun setNotificationButtonState(
+        isNotifyEnabled: Boolean?,
+        isUpdateEnabled: Boolean?,
+        isCancelEnabled: Boolean?
+    ) {
+        notify.isEnabled = isNotifyEnabled!!
+        update.isEnabled = isUpdateEnabled!!
+        cancel.isEnabled = isCancelEnabled!!
+    }
+
+    private fun cancelNotification() {
+        mNotifyManager.cancel(NOTIFICATION_ID)
+        setNotificationButtonState(true, false, false)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -101,5 +169,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         return true
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(notificationReceiver)
+        super.onDestroy()
     }
 }
